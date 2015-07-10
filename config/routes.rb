07bdf7f9e70cabeb6f5c76ded0f7b0d2dff4ec40
Cory <crbi2550@gmail.com>
2015-07-10 10:06:13 -0700

    Correctly adds user login and signup through Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   root 'home#index'
+
+  resources :users
 
   resources :skills
   resources :projects
